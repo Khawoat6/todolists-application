37ffdb7caf755ba4446015702fd749bc6924273b
oat phattaraphon <phattaraphon.c@ku.th>
2019-11-29 01:03:10 +0700

    adding LocalDatabase

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar,} from 'react-native';
-import StackNavigation from './stack/StackNavigation.js';
-import DrawerNavigation from './drawer/DrawerNavigation.js';
-import BottomNavigation from './bottom/BottomNavigation.js';
-import TopNavigation from './top/TopNavigation';
+// import StackNavigation from './stack/StackNavigation.js';
+// import DrawerNavigation from './drawer/DrawerNavigation.js';
+// import BottomNavigation from './bottom/BottomNavigation.js';
+// import TopNavigation from './top/TopNavigation';
 import SwitchNavigator from './job/SwitchNavigator';
 
 export default class App extends React.Component {
--- job/Page1.js
+++ job/Page1.js
@@ -1,25 +1,188 @@
-import React from 'react';
-import { StyleSheet, Text, View,Alert,TouchableOpacity} from 'react-native';
+import React from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import Constants from "expo-constants";
+import Items2 from './Items2'
+import Items3 from './Items3'
+import Items4 from './Items4'
+import { LinearGradient } from 'expo-linear-gradient'
+// import HeaderNavigationBar from './HeaderNavigationBar'
+import database from '../components/Database'
 
-export default class Page1 extends React.Component {
+
+export default class App extends React.Component {
 
 
-  onPressNext()
-  {
-    this.props.navigation.navigate('Page2Screen')
+  state = {
+    text: null
+  };
+
+  componentDidMount() {
+    this.update();
   }
+
+  onChangeText = text => this.setState({ text });
+
+  onPressAdd = () => {
+    this.addText();
+    this.setState({ text: null });
+  };
+
+  addText() {
+    if (this.state.text === null || this.state.text === "") {
+      return false;
+    }
+
+    database.putText(this.state.text, this.add_text_success, this.add_text_fail);
+
+
+  };
+
+  add_text_success=async()=>{
+    this.update();
+  }
+
+  add_text_fail=async(error)=>{
+    console.log(error);
+  }
+
+  change_Complete=(id)=>{
+    database.updateText2(id);
+    this.update();
+  }
+
+  change_Doing=(id)=>{
+    database.updateText(id);
+    this.update();
+  }
+
+
+  delete_Complete=(id)=>{
+    database.deleteText(id);
+    this.update();
+  }
+
+  update (){
+    this.todo.update();
+    this.todone.update();
+    this.todoing.update();
+  };
+
 
   render() {
     return (
-        <View style={{flex: 1,alignContent:'center'}} >
+      <LinearGradient
+       colors={['#7F7FD5', '#86A8E7', '#91EAE4']}
+       style={{flex: 1}}>
+         
+      <View style={{flex:1,justifyContent: 'center'}}>
+          <TextInput
+              style={styles.nameInput}
+              placeholder="What do you need to do ?"
+              onChangeText={this.onChangeText}/>
 
-        </View>
+          <TouchableOpacity
+              style={styles.touchableUser}
+              onPress={this.onPressAdd}>
+              <Text style={{fontSize:20, color:'#ffffff',textAlign:'center'}}>Create</Text>
+          </TouchableOpacity>
+
+          <ScrollView style={styles.listArea}>
+              <Items2
+                  ref={todo => (this.todo = todo)}
+                  onPressTodo={this.change_Doing}
+                    />
+              <Items3
+                  ref={todoing => (this.todoing = todoing)}
+                  onPressDoing={this.change_Complete}
+                    />
+              <Items4
+                  ref={todone => (this.todone = todone)}
+                  onPressComplate={this.delete_Complete}
+                    />
+        </ScrollView>
+      </View>
+      </LinearGradient>
     );
   }
 }
 
 const styles = StyleSheet.create({
-   btn:{
+  container: {
+    backgroundColor: "#fff",
+    flex: 1,
+    paddingTop: Constants.statusBarHeight
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  flexRow: {
+    flexDirection: "row"
+  },
+  input: {
+    borderColor: "#4630eb",
+    borderRadius: 4,
+    borderWidth: 1,
+    flex: 1,
+    height: 48,
+    margin: 16,
+    padding: 8
+  },
+  listArea: {
+    backgroundColor: "transparent",
+    flex: 1,
+    paddingTop: 16
+  },
+  sectionContainer: {
+    marginBottom: 16,
+    marginHorizontal: 16
+  },
+  sectionHeading: {
+    fontSize: 18,
+    marginBottom: 8
+  },
+  nameInput: {
+        alignItems: 'center',
+        height:50,
+        backgroundColor: 'transparent',
+        padding: 5,
+        margin:5,
+        borderRadius: 50,
+        borderColor:'white',
+        borderWidth : 1,
+        fontSize:20,
+    },
+    buttonText: {
+        alignItems: 'center',
+        height:50,
+        backgroundColor: '#86A8E7',
+        padding: 10,
+        margin:5,
+        borderRadius: 50,
+        borderColor:'white',
+        borderWidth : 1,
+        fontSize:30,
+        color:'#ffffff'
+    },
+    touchableUser:{
+      alignItems: 'center',
+      padding:10,
+      borderRadius: 50,
+      borderColor:'white',
+      borderWidth : 1,
+      margin:5,
+      marginTop:2,
+    },
+    btn:{
       alignItems: 'center',
       height:50,
       backgroundColor: '#86A8E7',
@@ -28,10 +191,9 @@ const styles = StyleSheet.create({
       borderRadius: 50,
       borderColor:'white',
       borderWidth : 1
-  },
-  txt:{
+    },
+    txt:{
       textAlign: 'center',
       fontSize:50
-  },
-
-})
+    },
+});
